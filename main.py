4d42ac7d80f507f83ab86da27e5df39d4e4e27af
KamiiilPawlak <kamil.partyk.pawlak@gmail.com>
2025-06-30 14:55:06 +0200

    create def log_operation()

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI, Query
 import numpy as np
+from datetime import datetime
 
+
+def log_operation(operation: str, num1: float, num2: float):
+    with open("log.txt", "a") as log:
+        timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        log.write(f"{timestamp} | {operation} | {num1} | {num2}\n")
 
 app = FastAPI()
+
+
 
 
 @app.get("/")
@@ -13,6 +21,7 @@ async def root():
 async def add(num1: float, num2: float):
     try:
         result = num1 + num2
+        log_operation("add", num1, num2)
         return {"Operation":"add","result":result}
     except Exception as e:
         return {"Error": str(e)}
@@ -22,6 +31,7 @@ async def subtract(num1: float, num2: float):
 
     try:
         result = num1 - num2
+        log_operation("subtract", num1, num2)
         return {"Operation": "subtract", "result": result}
     except Exception as e:
         return {"Error": str(e)}
@@ -30,6 +40,7 @@ async def subtract(num1: float, num2: float):
 async def multiply(num1: float, num2: float):
     try:
         result = num1 * num2
+        log_operation("multiply", num1, num2)
         return {"Operation": "multiply", "result": result}
     except Exception as e:
         return {"Error": str(e)}
@@ -40,27 +51,35 @@ async def divide(num1: float, num2: float):
         if num2 == 0:
             raise ValueError("divide by zero")
         result = num1 / num2
+        log_operation("divide", num1, num2)
         return {"Operation": "multiply", "result": result}
     except Exception as e:
         return {"Error": str(e)}
+
 @app.get("/trigonometric_function/sin")
 async def sin(num1: float = Query(..., description="Wartość w radianach, dowolna liczba")):
     try:
-        return {"operation": "sin", "result": np.sin(num1)}
+        result = np.sin(num1)
+        log_operation("sin", num1)
+        return {"operation": "sin", "result": result}
     except Exception as e:
         return {"error": str(e)}
 
 @app.get("/trigonometric_function/cos")
 async def cos(num1: float):
     try:
-        return {"operation": "cos", "result": np.cos(num1)}
+        result = np.cos(num1)
+        log_operation("cos", num1)
+        return {"operation": "cos", "result": result}
     except Exception as e:
         return {"error": str(e)}
 
 @app.get("/trigonometric_function/tan")
 async def tan(num1: float):
     try:
-        return {"operation": "tan", "result": np.tan(num1)}
+        result = np.tan(num1)
+        log_operation("tan", num1)
+        return {"operation": "tan", "result": result}
     except Exception as e:
         return {"error": str(e)}
 
@@ -68,6 +87,7 @@ async def tan(num1: float):
 async def asin(num1: float):
     try:
         result = np.arcsin(num1)
+        log_operation("asin", num1)
         return {"operation": "asin", "result": result}
     except Exception as e:
         return {"error": str(e)}
@@ -75,6 +95,8 @@ async def asin(num1: float):
 @app.get("/trigonometric_function/atan")
 async def atan(num1: float):
     try:
-        return {"operation": "atan", "result": np.arctan(num1)}
+        result = np.arctan(num1)
+        log_operation("atan", num1)
+        return {"operation": "atan", "result": result}
     except Exception as e:
         return {"error": str(e)}
