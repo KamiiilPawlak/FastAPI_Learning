7f39cb0a64972563a5df7f115799871a115a968c
KamiiilPawlak <kamil.partyk.pawlak@gmail.com>
2025-06-30 09:16:36 +0200

    add trigonometric_function

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
 from fastapi import FastAPI
+import numpy as np
+
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "this is a calculator :DD"}
+
+@app.get("/add")
+async def add(num1: float, num2: float):
+    return num1 + num2
+
+@app.get("/subtract")
+async def subtract(num1: float, num2: float):
+    return num1 - num2
+
+@app.get("/multiply")
+async def multiply(num1: float, num2: float):
+    return num1 * num2
+
+@app.get("/divide")
+async def divide(num1: float, num2: float):
+    return num1 / num2
+
+@app.get("/trigonometric_function/sin")
+async def sin(num1: float):
+    return np.sin(num1)
+
+@app.get("/trigonometric_function/cos")
+async def cos(num1: float):
+    return np.cos(num1)
+
+@app.get("/trigonometric_function/tan")
+async def tan(num1: float):
+    return np.tan(num1)
+
+@app.get("/trigonometric_function/asin")
+async def asin(num1: float):
+    return np.arcsin(num1)
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/trigonometric_function/atan")
+async def atan(num1: float):
+    return np.arctan(num1)
+
+
